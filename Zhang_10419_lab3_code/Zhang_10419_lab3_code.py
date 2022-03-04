146ab71befd9340d1be375544469948c9d8498b0
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-04 11:29:50 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -46,5 +46,5 @@ for n in range(5):
     year_list.append(year)
     total = df_tmp[year].sum()
     total_list.append(total)
-  chart_data = {'year':year_list,continent_list[n]:total_list}
+  chart_data = pd.DataFrame({continent_list[n]:total_list})
 chart_data

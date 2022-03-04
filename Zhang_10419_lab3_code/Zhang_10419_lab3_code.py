b0c55feb9d44d1c5b99b77711a5e3e18636a63bb
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-04 18:09:17 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -72,11 +72,12 @@ df_data_country = df_data_country.apply(pd.to_numeric, errors='coerce')
 country_stats = pd.DataFrame({'country': countries, 'mean': df_data_country.mean(axis=1),
                        'std': df_data_country.std(axis=1)})
 
-option1 = st.multiselect("select one country", countries)
+option1 = st.multiselect("select one country", countries,['Canada','Austria','India'])
 st.subheader("altair chart")
 
 chart_data = data.drop(columns=['Non-OECD Economies'])
 chart_data = pd.melt(chart_data, id_vars=['Country\year'], var_name='year')
+chart_data
 chart_data['value'] = chart_data['value'].apply(pd.to_numeric, errors='coerce')
 chart_data.rename(columns={"Country\year": "country", "value":"emission"}, inplace = True)
 chart_data

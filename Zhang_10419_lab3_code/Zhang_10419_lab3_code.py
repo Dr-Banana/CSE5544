07b5066b1b83a198a0d751acb10e01dcfca6979f
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-04 14:48:22 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -57,12 +57,9 @@ ax.set_ylabel("Year", fontsize = 15)
 
 st.pyplot(plt)
 
-data_url = 'https://raw.githubusercontent.com/CSE5544/data/main/ClimateData.csv'
-data = pd.read_csv("https://raw.githubusercontent.com/CSE5544/data/main/ClimateData.csv")
-data
-chart = alt.Chart(data_url).mark_circle().encode(
-    alt.X('Country\year'),
-    alt.Y('2019'),
+chart = alt.Chart(data).mark_circle().encode(
+    x='Country\year',
+    y='2019'
 )
 
 st.altair_chart(chart, use_container_width = True)

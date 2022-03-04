47b278dff7ff611153d495f07fe2ad9237f08bfa
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-04 17:44:25 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -92,38 +92,6 @@ heatmap = alt.Chart(chart_data).mark_rect().encode(
 st.altair_chart(heatmap, use_container_width = True)
 
 
-st.subheader("selectbox")
-option = st.selectbox("country", ("USA", "China", "Europe Union"))
-
-st.write(option)
-
-st.subheader("multi-select")
-option = st.multiselect("country", ("USA", "China", "Europe Union"), ("USA"))
-
-st.write(option)
-
-st.header("Interactive chart")
-
-st.subheader("interactive matplotlib chart")
-
-options = st.multiselect("select countries", countries, ['Australia'])
-
-country_stats.set_index("country", inplace=True)
-
-
-fig, ax = plt.subplots(figsize=(14, 6), dpi = 50)
-ax.bar(options, country_stats.loc[options]['mean'], yerr=country_stats.loc[options]['std'], capsize = 3)
-ax.set_axisbelow(True)  #ensure the grid is under the graph elements
-ax.margins(x=0.01) #set up the margin of graph
-ax.grid(alpha = 0.3) #show the grid line
-ax.set_xlabel('country')
-ax.set_ylabel('emissions')
-ax.set_title('The mean and std of emissions of countries')
-xaxis = plt.xticks(rotation=90, ha='center', fontsize=8)
-yaxis = plt.yticks(fontsize=8)
-
-st.pyplot(fig)
-
 st.subheader("interactive altair chart")
 
 option = st.selectbox("select one country", countries)

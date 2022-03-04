d2611e8b55ef5d5645da848593b4ca1c42259983
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-04 10:55:23 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -26,34 +26,5 @@ df = pd.DataFrame({
     'c2':[10,20,30,40]
 })
 
-data = pd.read_csv("https://raw.githubusercontent.com/CSE5544/data/main/ClimateData.csv")
+data = pd.read_csv("https://raw.githubusercontent.com/ZeningQu/World-Bank-Data-by-Indicators/master/gender/Metadata_Country_API_17_DS2_en_csv_v2_3058240.csv")
 data
-
-#prepare the data
-countries = data['Country\\year']
-df_data_country = data.iloc[:,2:]
-df_data_country = df_data_country.apply(pd.to_numeric, errors='coerce')
-country_stats = pd.DataFrame({'country': countries, 'mean': df_data_country.mean(axis=1),
-                       'std': df_data_country.std(axis=1)})
-
-#render results
-fig, ax = plt.subplots(figsize=(14, 6), dpi = 50)
-ax.bar(countries, country_stats['mean'], yerr=country_stats['std'], capsize = 3)
-ax.set_axisbelow(True)  #ensure the grid is under the graph elements
-ax.margins(x=0.01) #set up the margin of graph
-ax.grid(alpha = 0.3) #show the grid line
-ax.set_xlabel('country')
-ax.set_ylabel('emissions')
-ax.set_title('The mean and std of emissions of countries')
-xaxis = plt.xticks(rotation=90, ha='center', fontsize=8)
-yaxis = plt.yticks(fontsize=8)
-
-st.pyplot(fig)
-
-st.subheader("altair chart")
-
-chart_data = data.drop(columns=['Non-OECD Economies'])
-chart_data = pd.melt(chart_data, id_vars=['Country\year'], var_name='year')
-chart_data['value'] = chart_data['value'].apply(pd.to_numeric, errors='coerce')
-chart_data.rename(columns={"Country\year": "country", "value":"emission"}, inplace = True)
-chart_data

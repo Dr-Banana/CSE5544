5fb6db1367601e368b60004239d13a8ec7f86908
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-04 18:52:27 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -109,7 +109,7 @@ st.subheader("interactive altair chart")
 option = st.selectbox("select one country", countries)
 
 filter_data = chart_data[chart_data['country'] == option]
-bar_chart = alt.Chart(filter_data).mark_dot().encode(
+bar_chart = alt.Chart(filter_data).mark_circle().encode(
     x = 'year:O',
     y = 'emission:Q'
 )

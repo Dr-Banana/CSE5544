6bf326c202d4b50a1e2de46c334bbefce22c134a
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-04 18:04:34 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -72,7 +72,7 @@ df_data_country = df_data_country.apply(pd.to_numeric, errors='coerce')
 country_stats = pd.DataFrame({'country': countries, 'mean': df_data_country.mean(axis=1),
                        'std': df_data_country.std(axis=1)})
 
-option = st.selectbox("select one country", countries)
+option1 = st.multiselect("select one country", countries)
 st.subheader("altair chart")
 
 chart_data = data.drop(columns=['Non-OECD Economies'])

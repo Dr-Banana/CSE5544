008cf2c7acc6dc85d9b40ec35320cea871cbcff2
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-04 11:48:50 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -57,6 +57,5 @@ for n in range(5):
     total_list.append(total)
   chart_data[continent_list[n]] = total_list
 # display chart
-plt.imshow(chart_data,labels=dict(x="Continent",y="year",color="Productivity"),
-          x=continent_list,y=year_list
-          )
+plt.imshow(chart_data)
+plt.show()

0723483a2a7f8e70a9484e27f07fa31e48ed3edc
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-04 23:09:47 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -32,7 +32,7 @@ st.header("Step 1")
 singleSelect = st.selectbox("select one country", countries)
 df_tmp = pd.DataFrame()
 getList = data[data["Country\year"]== singleSelect].iloc[:,2:]
-a = getList[0].tolist()
+a = getList[0]
 a
 
 

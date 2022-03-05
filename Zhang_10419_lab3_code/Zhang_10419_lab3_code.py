9fddb279fbb36481663820d87aac88519b384231
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-04 22:55:08 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -32,8 +32,7 @@ st.header("Step 1")
 singleSelect = st.selectbox("select one country", countries)
 chart_data = pd.DataFrame()
 df_tmp = data[data["Country\year"]== singleSelect].iloc[:,2:].apply(pd.to_numeric, errors='coerce')
-singleBar = pd.DataFrame(index=singleSelect,{'mean': df_tmp.mean(axis=1),
-                       'std': df_tmp.std(axis=1)})
+singleBar = pd.DataFrame(data={'mean': df_tmp.mean(axis=1),'std': df_tmp.std(axis=1)},index=singleSelect)
 singleBar
 
 

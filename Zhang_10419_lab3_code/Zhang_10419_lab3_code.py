8874a1cea6a2ea9cc69a4e4a2aefd233d3e455ee
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-08 02:15:53 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -129,6 +129,37 @@ heatmap = alt.Chart(df_output).mark_rect().encode(
 st.altair_chart(heatmap, use_container_width = True)
 
 
+# Implementation of matplotlib function
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+import numpy as np
+	
+ang = 40
+rad = 10
+radm = 0.35
+radii = np.linspace(radm, 0.95, rad)
+	
+angles = np.linspace(0, 4 * np.pi, ang)
+angles = np.repeat(angles[..., np.newaxis], rad, axis = 1)
+angles[:, 1::2] += np.pi / ang
+	
+x = (radii * np.cos(angles)).flatten()
+y = (radii * np.sin(angles)).flatten()
+z = (np.sin(4 * radii) * np.cos(4 * angles)).flatten()
+	
+triang = tri.Triangulation(x, y)
+triang.set_mask(np.hypot(x[triang.triangles].mean(axis = 1),
+						y[triang.triangles].mean(axis = 1))
+					< radm)
+	
+tpc = plt.tripcolor(triang, z, shading ='flat')
+plt.colorbar(tpc)
+plt.inferno()
+plt.title('matplotlib.pyplot.inferno() function Example',
+									fontweight ="bold")
+plt.show()
+
+
 # st.subheader("interactive altair chart")
 
 # option = st.selectbox("select one country", countries)

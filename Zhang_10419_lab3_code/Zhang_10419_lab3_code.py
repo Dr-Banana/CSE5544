dac61e0cfbb34c386fad139fa6bc9c9a127d261f
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-03-08 16:40:03 -0500

    Update Zhang_10419_lab3_code.py

--- Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
+++ Zhang_10419_lab3_code/Zhang_10419_lab3_code.py
@@ -128,8 +128,8 @@ heatmap = alt.Chart(df_output).mark_rect().encode(
 st.altair_chart(heatmap, use_container_width = True)
 
 st.subheader("P2: dishonest/unethical/deceiving")
-
-chart_data = df_1
+data
+chart_data = data.rename(value-1990, axis='columns')
 chart_data = pd.melt(chart_data, id_vars=['Country\year'], var_name='year')
 chart_data['value'] = chart_data['value'].apply(pd.to_numeric, errors='coerce')
 chart_data.rename(columns={"Country\year": "country", "value":"emission"}, inplace = True)
@@ -148,8 +148,6 @@ option2 = st.multiselect("select country", countries,['Austria','India'])
 start_2,end_2 = st.slider('Select Year', 0, 30,(0,9))
 
 # Pick User choose countries
-chart_data['year']
-chart_data['year'] = [x for x in range(30)]
 year_choose = []
 for i in range(start_1,end_1):
     year = i

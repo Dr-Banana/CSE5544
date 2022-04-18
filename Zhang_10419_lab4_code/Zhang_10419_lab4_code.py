037c1cc283c987db14cf8a420eb73ff24fd68a3a
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-18 15:39:01 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -21,7 +21,7 @@ from vega_datasets import data
 
 def draw_map(mtype,y):
     
-    COLOR_THEME = {'count':"spectral"}
+    COLOR_THEME = {'count':"lighttealblue"}
     d['num'] = d[mtype]
     source = alt.topo_feature(data.world_110m.url, "countries")
     

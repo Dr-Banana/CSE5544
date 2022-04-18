a9b342c01c7e6b2093a8d2330bad282bf5a97a65
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-17 22:18:09 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -6,8 +6,6 @@ plt.rcParams['axes.edgecolor']='#FA6E4F'
 plt.rcParams['font.family'] = 'monospace'
 import seaborn as sns
 import geopandas as gpd
-import missingno as msno
-import re
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -16,3 +14,5 @@ import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
+
+university_df = pd.read_csv('../input/qs-world-university-rankings-2017-2022/qs-world-university-rankings-2017-to-2022-V2.csv')

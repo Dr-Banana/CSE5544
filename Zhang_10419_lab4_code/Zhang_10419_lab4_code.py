71d67b50d651ac2c03598d61f628a0a965af9be0
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-17 22:16:11 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -1,4 +1,18 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+plt.rcParams['axes.edgecolor']='#FA6E4F'
+plt.rcParams['font.family'] = 'monospace'
+import seaborn as sns
 import geopandas as gpd
+import missingno as msno
+import re
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))

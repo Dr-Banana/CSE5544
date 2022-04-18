3c89d921adc16a18e955755d1b5855df025b70c1
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-18 01:01:34 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -11,8 +11,8 @@ university_df = pd.read_csv("https://raw.githubusercontent.com/Dr-Banana/CSE5544
 university_df.set_index('country')
 st.dataframe(university_df)
 
-duplicateRows = university_df.duplicated(subset=['country'])
-st.dataframe(duplicateRows)
+duplicateRows = university_df.duplicated(subset=['country'], keep='first')
+st.dataframe(~duplicateRows)
 from vega_datasets import data
 
 # def draw_map(mtype='Total'):

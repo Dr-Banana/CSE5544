6e90f5c72561dd27a16aa2c4a46b56a6ddcf6154
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-17 22:41:17 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -4,5 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import geopandas as gpd
 
+st.title("Zhang_10419_lab3_code")
 university_df = pd.read_csv('https://raw.githubusercontent.com/Dr-Banana/CSE5544/main/Zhang_10419_lab4_code/qs-world-university-rankings-2017-to-2022-V2.csv')
 university_df.head()

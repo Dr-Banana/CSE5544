7141639b1995b95898488d3c69b6c63caa969721
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-18 13:02:04 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -20,32 +20,34 @@ university_df = pd.read_csv('https://raw.githubusercontent.com/Dr-Banana/CSE5544
 d = pd.DataFrame(university_df.pivot_table(columns=['country'], aggfunc='size'))
 d.columns = ['count']
 d['id'] = country_codes['Numeric']
+d.reset_index()
+d = d.rename(columns = {'index':'country'})
 st.dataframe(d)
-# from vega_datasets import data
+from vega_datasets import data
 
-# def draw_map(mtype='country'):
+def draw_map(mtype='count'):
     
-#     COLOR_THEME = {'country':"lightgreyred"}
+    COLOR_THEME = {'count':"lightgreyred"}
 
-#     source = alt.topo_feature(data.world_110m.url, "countries")
+    source = alt.topo_feature(data.world_110m.url, "countries")
 
-#     world_map = (
-#         alt.Chart(source, title=f'Countries by nunummber of {mtype} medals')
-#         .mark_geoshape(stroke="black", strokeWidth=0.15)
-#         .encode(
-#             color=alt.Color(
-#                 "num:N", 
-#                 scale=alt.Scale(scheme=COLOR_THEME[mtype]), 
-#                 legend=None),
-#             tooltip=[
-#                 alt.Tooltip("country:N", title="country"),
-#                 alt.Tooltip("count:Q", title="count"),
-#             ],
-#         )
-#         .transform_lookup(
-#             lookup="id",
-#             from_=alt.LookupData(olympic_medal_map, "id", ["Team/NOC", "Medals"]),
-#         )
-#     ).configure_view(strokeWidth=0).properties(width=700, height=400).project("naturalEarth1")
+    world_map = (
+        alt.Chart(source, title=f'Countries by nunummber of {mtype} medals')
+        .mark_geoshape(stroke="black", strokeWidth=0.15)
+        .encode(
+            color=alt.Color(
+                "num:N", 
+                scale=alt.Scale(scheme=COLOR_THEME[mtype]), 
+                legend=None),
+            tooltip=[
+                alt.Tooltip("country:N", title="country"),
+                alt.Tooltip("count:Q", title="count"),
+            ],
+        )
+        .transform_lookup(
+            lookup="id",
+            from_=alt.LookupData(olympic_medal_map, "id", ["Team/NOC", "Medals"]),
+        )
+    ).configure_view(strokeWidth=0).properties(width=700, height=400).project("naturalEarth1")
     
-#     return world_map
+    return world_map

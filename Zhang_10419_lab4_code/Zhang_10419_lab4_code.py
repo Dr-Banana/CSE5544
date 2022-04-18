f379c7f76f6e036693651768785aca1d2e9a430f
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-18 14:41:56 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -26,7 +26,7 @@ st.dataframe(d)
 YEAR = st.selectbox('Select a year',
                options = [2017,2018,2019,2020,2021,2022])
 year_university_df = university_df.loc[university_df['year'] == YEAR]
-st.dataframe(YEAR)
+st.dataframe(year_university_df)
 from vega_datasets import data
 def draw_map(mtype='count'):
     

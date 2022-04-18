2aae4841eadf3053e26a428ec1fe48575641506c
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-18 01:07:33 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -11,7 +11,7 @@ university_df = pd.read_csv("https://raw.githubusercontent.com/Dr-Banana/CSE5544
 university_df.set_index('country')
 st.dataframe(university_df)
 
-duplicateRows = university_df.drop_duplicated(subset=['country'], keep='FALSE')
+duplicateRows = university_df['country'].unique()
 st.dataframe(duplicateRows)
 from vega_datasets import data
 

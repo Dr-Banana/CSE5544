1cd54f4a56f1153aa835f89802e6e69cc0d33034
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-17 23:20:08 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -5,6 +5,14 @@ import seaborn as sns
 import geopandas as gpd
 import numpy as np
 import altair as alt
+import conda
+import os
+
+conda_file_dir = conda.__file__
+conda_dir = conda_file_dir.split('lib')[0]
+proj_lib = os.path.join(os.path.join(conda_dir, 'share'), 'proj')
+os.environ["PROJ_LIB"] = proj_lib
+
 
 st.title("Zhang_10419_lab3_code")
 # Set Up

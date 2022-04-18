73b5fa393e411fea1bdb671101265d1d87fe42b6
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-18 00:59:42 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -8,7 +8,7 @@ import altair as alt
 st.title("Zhang_10419_lab3_code")
 # Set Up
 university_df = pd.read_csv("https://raw.githubusercontent.com/Dr-Banana/CSE5544/main/Zhang_10419_lab4_code/qs-world-university-rankings-2017-to-2022-V2.csv")
-university_df.set_index('country', inplace = True)
+university_df.set_index('country')
 st.dataframe(university_df)
 
 duplicateRows = university_df[university_df.duplicated(subset=['country'])]

7d93947a58a3a120184e20d9041fa7b6b5959cb0
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-19 17:13:05 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -67,8 +67,8 @@ custom_palette1 = sns.color_palette(long_palette)
 research_size = pd.DataFrame(university_df.groupby(['research_output']).apply(lambda df: df['size'].value_counts()))
 research_size = research_size.reset_index().rename(columns={'level_1': 'size', 'size': 'count'})
 fig, ax = plt.subplots()
-sns.catplot(x="research_output", y="count", kind="point", data=research_size, hue='size', palette=custom_palette1);
-plt.xlabel('Research Output', fontsize=13, color = '#ff4800')
-plt.ylabel('Count', fontsize=13, color = '#ff4800')
-plt.title('Research output Vs Size of university', fontsize=15, color = '#ff4800');
+ax = sns.catplot(x="research_output", y="count", kind="point", data=research_size, hue='size', palette=custom_palette1);
+ax.xlabel('Research Output', fontsize=13, color = '#ff4800')
+ax.ylabel('Count', fontsize=13, color = '#ff4800')
+ax.title('Research output Vs Size of university', fontsize=15, color = '#ff4800');
 st.pyplot(fig)

62bf3b2e2605e145d66c83a6aaac0f8945ffb4e7
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-19 17:53:39 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -66,9 +66,8 @@ st.write(draw_map('count',YEAR))
 uni_df = year_university_df['university'].value_counts()
 
 fig, ax = plt.subplots(figsize=(10,20), dpi=150)
-
+ax = sns.color_palette("light:#5A9", as_cmap=True)
 ax = sns.countplot(data=year_university_df, y='country', order=year_university_df.country.value_counts().index)
-sns.color_palette("light:#5A9", as_cmap=True)
 plt.xlabel('Number of universities', fontsize=12, color = '#ff4800')
 plt.ylabel('Country', fontsize=12, color = '#ff4800')
 plt.title("Distribution of universities across countries", fontsize=14, color = '#ff4800');

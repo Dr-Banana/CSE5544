4cb563799bb638cb8fb27319bdc9f1321183ad72
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-20 11:30:56 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -97,7 +97,7 @@ qs_data['rank_display'] = qs_data['rank_display'].apply(lambda x: float(re.sub(r
 panel1 = st.container()
 with panel1:
     # Create 3 widgets to change conditions to filter data
-    columns = st.columns([2.1, 0.3, 2.1, 0.3, 2.1, 0.3, 2.1])
+    columns = st.columns([1.1, 0.3, 3.1, 0.3, 2.1, 0.3, 2.1])
     # For the first widget, we create a slider to select years
     with columns[2]:
         df1 = qs_data.copy()

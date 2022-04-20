080097b24d8c700d4586a1be0f7637401ada20e6
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-20 12:05:18 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -188,7 +188,7 @@ def draw_map(mtype,y):
             lookup="id",
             from_=alt.LookupData(d, "id", ["country", "num"]),
         )
-    ).configure_view(strokeWidth=0).properties(width=2000, height=400)
+    ).configure_view(strokeWidth=0).properties(width=800, height=400)
     
     
     return world_map

be9f471939c62e2baed33923a53708f8e0916414
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-20 12:10:02 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -219,3 +219,25 @@ fig = px.treemap(df_plt, path=[px.Constant("world"), 'region', 'country'], value
                   color_continuous_scale='RdBu')
 
 st.plotly_chart(fig, use_container_width=True)
+
+# ---------------------------------------------Hayden---------------------------------------------
+
+data = pd.read_csv("https://github.com/hwahoh/CSE5544-lab-final/blob/438797bb4119358b632991281763ec24273a9b45/qs-world-university-rankings-2017-to-2022-V2.csv?raw=true")
+
+dv1 = data.drop(columns=['link','logo'])
+dv1['score'] = dv1['score'].apply(pd.to_numeric, errors='coerce')
+#dv1['year'] = dv1['year'].apply(pd.to_numeric, errors='coerce', downcast='integer')
+regions = dv1['region'].drop_duplicates()
+region_choice = st.selectbox('Select your universities:', regions)
+countries = dv1['country'].loc[dv1['region'] == region_choice].drop_duplicates()
+country_choice = st.selectbox('', countries)
+
+dv1 = dv1[dv1['country'] == country_choice]
+chart1 = alt.Chart(dv1).mark_line().encode(
+    x='year:N',
+    y='score',
+    color='university',
+    tooltip= ['year', 'university', 'score','student_faculty_ratio', 'type', 'research_output']
+)
+
+st.altair_chart(chart1, use_container_width=True)

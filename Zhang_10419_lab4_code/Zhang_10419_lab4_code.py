0b6afd3b2e887904779a75525ffebaf856628a78
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-04-20 11:56:58 -0400

    Update Zhang_10419_lab4_code.py

--- Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
+++ Zhang_10419_lab4_code/Zhang_10419_lab4_code.py
@@ -203,9 +203,8 @@ d = pd.DataFrame(year_university_df.pivot_table(columns=['country'], aggfunc='si
 d.columns = ['count']
 d['id'] = country_codes['Numeric']
 d['country'] = d.index
-
-st.plotly_chart(draw_map('count',start_year), use_container_width=True)
-# st.write(draw_map('count',start_year))
+fig = draw_map('count',start_year)
+st.write(draw_map('count',start_year))
 
 # ---------------------------------------------Shloksah---------------------------------------------
 df =  pd.read_csv("https://raw.githubusercontent.com/CristoDragon/CSE5544-Lab3/main/QS_ranking.csv" ,sep=',', encoding='latin-1')
